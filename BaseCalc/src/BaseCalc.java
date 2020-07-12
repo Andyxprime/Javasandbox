85070dcd1532d2178cb56b0b4343e7147e2848b0
Andy Balakan <andy.balakan@gmail.com>
2020-07-12 18:42:19 +0300

    BaseCalc commit

---
+++ BaseCalc/src/BaseCalc.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class BaseCalc {
+	
+public static void main(String[] args) {
+	
+	System.out.println("Greetings! BaseCalc is launched.");
+	
+	Scanner command = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
+	String text;
+	double A;
+	double B;
+	
+	do {
+	System.out.println("\nPlease enter the command to start('help' for list of commands):");
+	text = command.nextLine();
+	
+	switch (text) {
+	case "help":
+		System.out.println("'help' - list of commands");
+		System.out.println("'add' or '+' - start add operation");
+		System.out.println("'sub' or '-' - start substract operation");
+		System.out.println("'mult' or '*' - start multiply operation");
+		System.out.println("'div' or '/' - start divide operation");
+		System.out.println("'exit' or 'X' - exit the app");
+		break;
+	
+	case "add":
+	case "+":
+	System.out.println("Enter first variable:");
+	A = input.nextInt();
+	System.out.println("Enter second variable:");
+	B = input.nextInt();
+	System.out.println("The result is: " + (A + B));
+	break;
+	
+	case "sub":
+	case "-":
+		System.out.println("Enter first variable:");
+		A = input.nextInt();
+		System.out.println("Enter second variable:");
+		B = input.nextInt();
+		System.out.println("The result is: " + (A - B));
+		break;
+		
+	case "mult":
+	case "*":
+		System.out.println("Enter first variable:");
+		A = input.nextInt();
+		System.out.println("Enter second variable:");
+		B = input.nextInt();
+		System.out.println("The result is: " + (A * B));
+		break;
+		
+	case "div":
+	case "/":
+		System.out.println("Enter first variable:");
+		A = input.nextInt();
+		System.out.println("Enter second variable:");
+		B = input.nextInt();
+		if(B==0) {
+			System.out.println("Dividing by zero is forbidden. Enter a valid variable.");
+			System.out.println("Enter second variable:");
+			B = input.nextInt();
+		}
+		System.out.println("The result is: " + (A / B));
+		break;
+		
+	case "exit":
+	case "X":	
+		System.out.println("Exiting now...");
+		System.exit(0);
+		
+		default:
+			System.out.println("Command not recognized. Please enter another command or type 'help' for list of commands.");
+	}
+}
+	while(text != "exit"); 
+		command.close();
+		input.close();
+}
+}
